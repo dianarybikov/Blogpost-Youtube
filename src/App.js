6c70ac85790eb4c8d4d65b585b0686a69703d186
Diana Rybikov <diana.ryb214@gmail.com>
2021-11-05 10:50:42 -0600

    use effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import { useEffect } from 'react'
 import './App.css';
 
 function App() {
+
+
+  useEffect( () =>{
+    fetch('http://localhost:3000/pirates')
+    .then (response => response.json())
+    .then(pirates => setPirates(pirates))
+  })
   return (
     <div className="App">
       
